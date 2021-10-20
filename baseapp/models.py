10cb67e00461ff8d08596c88a598ceacfdbc7554
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-10-20 22:20:51 +0300

    backend comm 1

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product=models.CharField(max_length=255)
+    category=models.CharField(max_length=255)
+    quantity=models.CharField(max_length=255)
+    price=models.DecimalField(decimal_places=2,max_digits=7)
+
+    def __str__(self):
+        return self.product
--- baseapp/urls.py
+++ baseapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('management',views.manage,name='manage'),
-    path('edit-stock',views.editstock,name='editstock'),
+    path('management/',views.ManageStock.as_view(),name='manage'),
+    path('add-new-stock/',views.Addnewstock.as_view(),name='addnewstock'),
+    path('edit-stock/<int:pk>/',views.UpdateProduct.as_view(),name='editstock')
 ]
